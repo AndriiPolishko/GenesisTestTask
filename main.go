7356f9d6bfcb38e4ed29a0514ae89f56917113d5
AndriiPolishko <ramp8a799@gmail.com>
2023-05-28 16:33:23 +0300

    add service logic

--- main.go
+++ main.go
@@ -34,13 +34,40 @@ func getBTCPriceInUAH(url string) (int, error) {
 	return btcPrice.MarketData.CurrentPrice.UAH, nil
 }
 
-func main() {
-	log.SetPrefix("Main error")
+type RateResponse struct {
+	Rate int `json:"rate"`
+}
+
+func rateHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the logic to retrieve the BTC to UAH rate from a third-party service
 
 	var btcAPIurl string = "https://api.coingecko.com/api/v3/coins/bitcoin"
-	priceOfBTCInUAH, err := getBTCPriceInUAH(btcAPIurl)
+	rate, err := getBTCPriceInUAH(btcAPIurl)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(priceOfBTCInUAH)
+
+	response := RateResponse{
+		Rate: rate,
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error marshaling JSON response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
+
+func main() {
+	log.SetPrefix("Main error")
+
+	http.HandleFunc("/api/rate", rateHandler)
+
+	fmt.Println("Server listening on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
